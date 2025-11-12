beb27c1bdea3dc32c82831f3790a8af9fafebbac
augustinlss <augustin.zhlass@gmail.com>
2025-11-13 00:08:41 +0100

    added some stuff

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 use std::env;
 
+use anyhow::Result;
+use clap::Parser;
+use game::state::{start_game, Game};
+
 mod game;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    count: usize,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let words_count = args.count;
+
+    let game: Game = start_game(words_count)?;
+
+    Ok(())
 }
